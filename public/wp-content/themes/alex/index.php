0e1d19a73fb9838f098df9216444907a7ed235d8
Martin Sonnberger <msonnberger.mmt-b2020@fh-salzburg.ac.at>
2021-10-27 15:34:57 +0200

    setup for custom theme

---
+++ public/wp-content/themes/alex/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <?php wp_title() ?>
+  <?php wp_head() ?>
+</head>
+<body>
+  <?php wp_body_open() ?>
+
+  <h1>Hello Alex</h1>
+
+  <?php wp_footer() ?>
+</body>
+</html>
