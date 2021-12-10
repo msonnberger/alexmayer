885166880b3a0de1c4b1e134c6b53b392ab97b60
Martin Sonnberger <msonnberger.mmt-b2020@fh-salzburg.ac.at>
2021-12-10 15:14:07 +0100

    add deploy script

---
+++ deploy.php
@@ -0,0 +1,53 @@
+<?php
+namespace Deployer;
+
+require 'recipe/common.php';
+
+// Project name
+set('application', 'wpalex');
+
+// Project repository
+set('repository', 'https://github.com/msonnberger/alexmayer.git');
+
+// [Optional] Allocate tty for git clone. Default value is false.
+set('git_tty', true); 
+
+// Shared files/dirs between deploys 
+set('shared_files', ['public/wp-config.php']);
+set('shared_dirs', ['public/wp-content/uploads']);
+
+// Writable dirs by web server 
+//set('writable_dirs', []);
+set('allow_anonymous_stats', false);
+
+// Hosts
+
+host('vm-amber.multimediatechnology.at')
+    ->user('admin')
+    ->port(5412)
+    ->set('deploy_path', '/home/admin/alex');    
+    
+
+// Composer
+set('composer_action', false);
+
+// Tasks
+
+desc('Deploy your project');
+task('deploy', [
+    'deploy:info',
+    'deploy:prepare',
+    'deploy:lock',
+    'deploy:release',
+    'deploy:update_code',
+    'deploy:shared',
+    'deploy:writable',
+    'deploy:clear_paths',
+    'deploy:symlink',
+    'deploy:unlock',
+    'cleanup',
+    'success'
+]);
+
+// [Optional] If deploy fails automatically unlock.
+after('deploy:failed', 'deploy:unlock');
