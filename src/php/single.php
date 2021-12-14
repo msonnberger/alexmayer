6f89213d987a99f66b306842bc91a0f026a6f990
Martin Sonnberger <msonnberger.mmt-b2020@fh-salzburg.ac.at>
2021-12-14 14:57:32 +0100

    add contact form plugin

---
+++ src/php/single.php
@@ -0,0 +1,10 @@
+<?php include 'head.php' ?>
+<?php include 'header.php' ?>
+</header>
+<main>
+  <section class="max-width-container">
+    <h2><?php the_title() ?></h2>
+    <?php the_content() ?>
+  </section>
+</main>
+<?php include 'footer.php' ?>
